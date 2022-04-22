80bd6e9f18607864865ab77fb8e690f98d44c674
Kanupriya <kanupriyasanjay.agrawal@sjsu.edu>
2022-04-22 13:00:37 -0700

    feat: 22 - Implemented Room CRUD APIs

--- EpicHotelBookingService/backend/src/main/java/com/hotelbooking/HotelBookingApplication.java
+++ EpicHotelBookingService/backend/src/main/java/com/hotelbooking/HotelBookingApplication.java
@@ -1,40 +1,23 @@
 package com.hotelbooking;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Slf4j
 public class HotelBookingApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    SpringApplication.run(HotelBookingApplication.class, args);
+  }
 
-		SpringApplication.run(HotelBookingApplication.class, args);
-	}
-
-	/*
-	Components
-APIs - input and output of API should be in JSON and should include error handling and validation of inputs
-APIs can be demonstrated using a basic Web/mobile UI or through Postman
-UI will be used by Customers and Hotel employees
-APIs should support following functionality:
-Manage your Hotel rewards account
-Search for Hotels
-Book one or more rooms for stay up to 1 week
-Options may be selected for each room for one or more amenities:
-Daily Continental Breakfast
-Access to fitness room
-Access to Swimming Pool/Jacuzzi
-Daily Parking
-All meals included (Breakfast, Lunch, Dinner)
-Room rates based on Room types and number of guests e.g. Double rooms, Suites, Singe room ( up to guests)
-Use dynamic pricing - based on weekdays/weekends/holidays
-Seasonal - peak season such as Summer/Christmas Holiday season
-Customer Loyalty
-Change/Cancel reservations
-Enrolling as a new customer
-Deploy API to AWS in an Auto Scaled EC2 Cluster with Load Balancer (or another cloud provider)
-Develop a Web or mobile UI that will make use of the APIs
-Create your own database with mock data
-	 */
-
+  @GetMapping("/health")
+  public String healthCheck() {
+    log.info("Health check being done");
+    return "Health is OK";
+  }
 }
--- EpicHotelBookingService/backend/src/main/java/com/hotelbooking/controller/RoomController.java
+++ EpicHotelBookingService/backend/src/main/java/com/hotelbooking/controller/RoomController.java
@@ -1,2 +1,47 @@
-package com.hotelbooking.controller;public class RoomController {
+package com.hotelbooking.controller;
+
+import com.hotelbooking.models.Hotel;
+import com.hotelbooking.models.Room;
+import com.hotelbooking.service.HotelService;
+import com.hotelbooking.service.RoomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class RoomController {
+
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @PostMapping("/rooms")
+    public void addRoom(@RequestBody Room room) {
+
+        roomService.addRoom(room);
+    }
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+
+    @GetMapping("/rooms/{id}")
+    public Optional<Room> getRoomById(@PathVariable Integer id)  {
+        return roomService.getRoomById(id);
+    }
+
+    @PutMapping("/rooms/{id}")
+    public void updateRoomDetails(@RequestBody Room room, @PathVariable Integer id) {
+        roomService.updateRoomDetails(room, id);
+    }
+
+    @DeleteMapping("/rooms/{id}")
+    public void deleteRoom(@PathVariable Integer id) {
+        roomService.deleteRoom(id);
+    }
 }
