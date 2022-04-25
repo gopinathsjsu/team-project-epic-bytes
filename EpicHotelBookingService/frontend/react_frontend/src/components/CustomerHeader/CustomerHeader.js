8f4e3fb40b84e3eb7f5517d690ef96a52b9082b9
Rishab Karakala <rishabreddy.0048@gmail.com>
2022-04-24 18:04:41 -0700

    implemented home page

--- EpicHotelBookingService/frontend/react_frontend/src/components/CustomerHeader/CustomerHeader.js
+++ EpicHotelBookingService/frontend/react_frontend/src/components/CustomerHeader/CustomerHeader.js
@@ -30,45 +30,6 @@ export const CustomerHeader = (props) => {
     history.push("/login");
   };
 
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "20ch",
-      },
-    },
-  }));
 
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -158,19 +119,10 @@ export const CustomerHeader = (props) => {
 
   return (
     <>
-      <Box sx={{ flexGrow: 1, marginBottom: "2.25rem" }}>
+      <Box sx={{ flexGrow: 1 }}>
         <AppBar sx={styles} position="static">
           <Toolbar>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-                onInput={onSearchInputChange}
-              />
-            </Search>
+
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ display: { xs: "none", md: "flex" } }}>
               <IconButton
