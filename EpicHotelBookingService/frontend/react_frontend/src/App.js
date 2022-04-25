388f97687a7c274e50db9bfdccb5dc116c2201b6
Rishab Karakala <rishabreddy.0048@gmail.com>
2022-04-24 17:41:27 -0700

    implemented home page

--- EpicHotelBookingService/frontend/react_frontend/src/App.js
+++ EpicHotelBookingService/frontend/react_frontend/src/App.js
@@ -1,9 +1,17 @@
+import { dividerClasses } from "@mui/material";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
 import { Routes } from "./Routes";
 import { AppContext, AppContextComponent } from "./store/appContext";
 
 export const App = () => {
   const contextComponent = AppContextComponent();
   return (
+    // <div>
+
+    //    <Navbar />
+    //    <Hero />
+    // </div>
     <AppContext.Provider value={contextComponent}>
       <Routes />
     </AppContext.Provider>
