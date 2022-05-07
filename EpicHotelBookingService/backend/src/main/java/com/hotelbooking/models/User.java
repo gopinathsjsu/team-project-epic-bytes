d6e9d1114e77d233aee52f4469f58906e3b82da9
Kanupriya <kanupriyasanjay.agrawal@sjsu.edu>
2022-05-07 01:55:28 -0700

    code refactored

--- EpicHotelBookingService/backend/src/main/java/com/hotelbooking/models/User.java
+++ EpicHotelBookingService/backend/src/main/java/com/hotelbooking/models/User.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Table(indexes = {@Index(columnList = "email", unique = true)})
