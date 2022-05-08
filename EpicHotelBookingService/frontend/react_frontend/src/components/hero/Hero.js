9a7b5afabeb61a3d9671613141ba9dd209981285
Kanupriya <kanupriyasanjay.agrawal@sjsu.edu>
2022-05-07 22:05:18 -0700

    Fixed search

--- EpicHotelBookingService/frontend/react_frontend/src/components/hero/Hero.js
+++ EpicHotelBookingService/frontend/react_frontend/src/components/hero/Hero.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './HeroStyles.css'
-import Video from '../../assets/maldivesVideo.mp4'
 
 import {
     faBed,
@@ -56,9 +55,6 @@ const Hero = ({ type }) => {
 
     return (
         <div className='hero'>
-            <video autoPlay loop muted id='video'>
-                <source src={Video} type='video/mp4' />
-            </video>
             <div className="overlay"></div>
             <div className="content">
                 <h1>First Class Travel</h1>
@@ -178,7 +174,5 @@ const Hero = ({ type }) => {
         </div>
     )
 }
- 
-
 
 export default Hero
